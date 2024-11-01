c93db755f786744865afac3b02e246d0b56cc2fd
GuilhermeFT <guilhermetrind12@gmail.com>
2024-11-01 15:41:35 -0300

    fix: add start command

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import Fastify from "fastify";
+import Fastify from "fastify"
 
 const fastify = Fastify({
   logger: true,
-});
+})
 
 fastify.get("/", (request, reply) => {
-  reply.send({ hello: "world" });
-});
+  reply.send({ hello: "world" })
+})
 
 fastify.listen({ port: 3333 }, (err) => {
   if (err) {
-    fastify.log.error(err);
-    process.exit(1);
+    fastify.log.error(err)
+    process.exit(1)
   }
-});
+})
